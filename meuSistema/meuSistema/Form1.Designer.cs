1f3b271955171e4de796cd6611082a8221747080
JorgeLucas <83237763+PlacideBoy@users.noreply.github.com>
2023-04-30 20:58:38 -0300

    update

--- meuSistema/meuSistema/Form1.Designer.cs
+++ meuSistema/meuSistema/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace meuSistema
 {
-    partial class Form1
+    partial class FrmMeuSistema
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(24, 198);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(141, 32);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(568, 198);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(159, 32);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FrmMeuSistema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Name = "FrmMeuSistema";
+            this.Text = "Aplicação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
 
